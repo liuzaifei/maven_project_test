dd8f6940393d9c1124a3c40006fc39f9b507ea95
liuzaifei <461306424@qq.com>
2018-04-07 20:24:13 +0800

    这是maven上传项目测试

---
+++ api/src/main/java/constants/CommonConstants.java
@@ -0,0 +1,11 @@
+package constants;
+
+public class CommonConstants {
+	
+	/** 用户登录状态 等待*/
+	public final static int USER_LOGIN_STATUS_AUDIT = -1;
+	
+	/** 用户登录状态 通过*/
+	public final static int USER_LOGIN_STATUS_CONFIRM = 1;
+	
+}
